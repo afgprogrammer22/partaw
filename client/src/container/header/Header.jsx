83ab2f21b5f2d06bab85b895c8d22141e8eb1917
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-06-01 14:31:01 +0530

    header added

---
+++ client/src/container/header/Header.jsx
@@ -0,0 +1,18 @@
+import './header.scss'
+import { images } from '../../constants'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header__image'>
+            <img src={images.background} alt="" />
+            <div className='header__info'>
+              <h1>Partaw Association</h1>
+              <h4>Partaw is a group of students in Pune, India. The Association is trying to show the best face of Afghan students to other nations. Partaw is an association that include best, hardworking, talent and motivated students from Afghanistan.</h4>
+              <button>More Info</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
