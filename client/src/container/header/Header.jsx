495b87e933083b1bd53d62bd13b669f64e8ef7d9
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-06-17 07:46:03 +0530

    Add some changes

--- client/src/container/header/Header.jsx
+++ client/src/container/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className='header__info'>
               <h1>Partaw Association</h1>
               <h4>Partaw is a group of students in Pune, India. The Association is trying to show the best face of Afghan students to other nations. Partaw is an association that include best, hardworking, talent and motivated students from Afghanistan.</h4>
-              <button>More Info</button>
+              <button>Read More</button>
             </div>
         </div>
     </div>
